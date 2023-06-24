1936c9a6c63b1f67ab868f53935b7091d84138ef
laurenor <6752700+laurenor@users.noreply.github.com>
2023-06-24 13:43:39 -0700

    Set up form reset

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import "./App.css";
 import BingoCard from "./BingoCard";
 import { useState } from "react";
 
+const DEFAULT_BINGO_VALUES = [];
 const DEFAULT_NUM_CARDS = 1;
 const DEFAULT_TITLE = "BINGO";
+const DEFAULT_FREE_TYPE = "default";
 
 function App() {
-  const [bingoValues, setBingoValues] = useState([]);
+  const [bingoValues, setBingoValues] = useState(DEFAULT_BINGO_VALUES);
   const [numCards, setNumCards] = useState(DEFAULT_NUM_CARDS);
   const [title, setTitle] = useState(DEFAULT_TITLE);
+  const [freeType, setFreeType] = useState(DEFAULT_FREE_TYPE);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
-    setBValues(formJson.bingoValues);
+    setBingoValues(setBValues(formJson.bingoValues));
     setTitle(formJson.title);
+    setFreeType(formJson.freeType);
     setNumCards(parseInt(formJson.numCards));
+  };
+
+  const handleReset = (e) => {
+    setBingoValues(DEFAULT_BINGO_VALUES);
+    setTitle(DEFAULT_TITLE);
+    setFreeType(DEFAULT_FREE_TYPE);
+    setNumCards(DEFAULT_NUM_CARDS);
   };
 
   const setBValues = (values) => {
@@ -25,39 +36,54 @@ function App() {
       .split(",")
       .map((value) => value.trim())
       .filter((value) => value.length > 0);
-    setBingoValues(parsedValues);
     return parsedValues;
   };
 
   return (
     <div className="app">
-      <form onSubmit={handleSubmit} class="bingo-form">
-        <label>
-          <div className="label-name">Title: </div>
-          <input name="title" defaultValue={DEFAULT_TITLE} />
-        </label>
+      <div class="controls">
+        <button>Start Game</button>
+        <form onSubmit={handleSubmit} onReset={handleReset} class="bingo-form">
+          <label>
+            <div className="label-name">Title: </div>
+            <input name="title" defaultValue={DEFAULT_TITLE} />
+          </label>
 
-        <label>
-          <div className="label-name">Num cards: </div>
-          <input
-            name="numCards"
-            type="number"
-            defaultValue={DEFAULT_NUM_CARDS}
-            min="1"
-          />
-        </label>
+          <label>
+            <div className="label-name">Free spot type: </div>
+            <select name="freeType" id="freeType">
+              <option value="default">Default</option>
+              <option value="remy">Remy</option>
+            </select>
+          </label>
 
-        <label>
-          <div className="label-name">Bingo Values (comma separated): </div>{" "}
-          <textarea name="bingoValues" defaultValue="" />
-        </label>
+          <label>
+            <div className="label-name">Num cards: </div>
+            <input
+              name="numCards"
+              type="number"
+              defaultValue={DEFAULT_NUM_CARDS}
+              min="1"
+            />
+          </label>
 
-        <button type="reset">Reset form</button>
-        <button type="submit">Submit form</button>
-      </form>
+          <label>
+            <div className="label-name">Bingo Values (comma separated): </div>{" "}
+            <textarea name="bingoValues" defaultValue="" />
+          </label>
+
+          <button type="reset">Reset form</button>
+          <button type="submit">Submit form</button>
+        </form>
+      </div>
 
       {[...Array(numCards)].map((e, i) => (
-        <BingoCard title={title} values={bingoValues} key={`card${i}`} />
+        <BingoCard
+          freeType={freeType}
+          title={title}
+          values={bingoValues}
+          key={`card${i}`}
+        />
       ))}
     </div>
   );
--- src/BingoCard.jsx
+++ src/BingoCard.jsx
@@ -1,43 +1,70 @@
 import { useState, useEffect } from "react";
 import { shuffle } from "./utils";
-export default function BingoCard({ values, title }) {
-  const [cardVals, setCardVals] = useState([
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "FREE", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-  ]);
+import RemyBingo from "./img/remy_bingo.png";
+
+const DEFAULT_CARD_VALUES = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "FREE", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export default function BingoCard({ values, title, freeType }) {
+  const [cardVals, setCardVals] = useState(DEFAULT_CARD_VALUES);
 
   // initialize vals
   useEffect(() => {
-    if (values.length < 2) return;
-    const shuffledVals = shuffle([...values]);
-    const cardValsCopy = [...cardVals];
-    for (let i = 0; i < cardVals.length; i++) {
-      const row = cardVals[i];
-      for (let j = 0; j < row.length; j++) {
-        const val = row[j];
-        if (val !== "FREE") {
-          if (shuffledVals.length) {
-            cardVals[i][j] = shuffledVals.pop();
-          } else {
-            break;
+    if (values.length < 2) {
+      console.log("HELLO", values.length);
+      if (values.length < 1) {
+        console.log("should reset");
+        setCardVals(DEFAULT_CARD_VALUES);
+      }
+    } else {
+      const shuffledVals = shuffle([...values]);
+      const cardValsCopy = [...cardVals];
+      for (let i = 0; i < cardVals.length; i++) {
+        const row = cardVals[i];
+        for (let j = 0; j < row.length; j++) {
+          const val = row[j];
+          if (val !== "FREE") {
+            if (shuffledVals.length) {
+              cardVals[i][j] = shuffledVals.pop();
+            } else {
+              break;
+            }
           }
         }
       }
+      setCardVals(cardValsCopy);
     }
-    setCardVals(cardValsCopy);
-  }, [values, cardVals]);
+  }, [values]);
+
+  useEffect(() => {
+    console.log({ cardVals });
+  }, [cardVals]);
 
   return (
     <div class="bingo-card">
-      <div class="bingo-header bingo-row">{title}</div>
-      {cardVals.map((row) => {
+      <div class="bingo-header">{title}</div>
+      {cardVals.map((row, i) => {
         return (
-          <div class="bingo-row">
-            {row.map((val) => {
-              return <div class="bingo-val">{val}</div>;
+          <div class="bingo-row" key={`row${i}`}>
+            {row.map((val, j) => {
+              return (
+                <div class="bingo-val" key={`val${j}`}>
+                  {val === "FREE" && freeType === "remy" ? (
+                    <img
+                      src={RemyBingo}
+                      alt="Remy Free Spot"
+                      class="free-img"
+                    />
+                  ) : (
+                    val
+                  )}
+                </div>
+              );
             })}
           </div>
         );
